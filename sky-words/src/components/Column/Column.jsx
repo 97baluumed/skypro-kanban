d802c5be0c3766c7b495e902d8ca47e3d4bfe2ea
максим <notolerances@yandex.ru>
2025-11-30 15:50:44 +0300

    потно

---
+++ sky-words/src/components/Column/Column.jsx
@@ -0,0 +1,12 @@
+export function Column({ title, children }) {
+    return (
+        <div className="main__column">
+            <div className="column__title">
+                <p>{title}</p>
+            </div>
+            <div className="cards">
+                {children}
+            </div>
+        </div>
+    );
+}
